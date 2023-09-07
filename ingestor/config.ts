4b36eb075b4f613b3d07b2702a949d39bc0b4485
Shreyan Jain <shreyan@eladgil.com>
2023-09-07 16:40:30 -0700

    rename

--- ingestor/config.ts
+++
@@ -1,6 +0,0 @@
-// OpenAI
-export const OPEN_AI_KEY: string = process.env.OPENAI_API_KEY as string
-
-// Supabase
-export const SUPABASE_URL: string = process.env.SUPABASE_URL as string
-export const SUPABASE_SERVICE_KEY: string = process.env.SUPABASE_SERVICE_KEY as string
