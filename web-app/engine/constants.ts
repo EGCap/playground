2666a055f702cd6d0c3eba62ef77f540d3201738
Shreyan Jain <2shreyan.jain@gmail.com>
2023-09-21 12:56:51 -0700

    alphabetize

--- web-app/engine/constants.ts
+++ web-app/engine/constants.ts
@@ -2,13 +2,13 @@ import { EMBEDDING_MODEL } from "./types";
 
 // Add additional embedding models to enable here
 export const embeddingModelsInProd: EMBEDDING_MODEL[] = [
-  EMBEDDING_MODEL.OPEN_AI,
-  EMBEDDING_MODEL.MPNET_BASE_V2,
   EMBEDDING_MODEL.INSTRUCTOR_LARGE,
+  EMBEDDING_MODEL.MPNET_BASE_V2,
+  EMBEDDING_MODEL.OPEN_AI,
 ];
 
 export const userFriendlyNameByModel = new Map(Object.entries({
-  [EMBEDDING_MODEL.OPEN_AI]: "text-ada-002",
-  [EMBEDDING_MODEL.MPNET_BASE_V2]: "mpnet-base-v2",
   [EMBEDDING_MODEL.INSTRUCTOR_LARGE]: "instructor-large",
+  [EMBEDDING_MODEL.MPNET_BASE_V2]: "mpnet-base-v2",
+  [EMBEDDING_MODEL.OPEN_AI]: "text-ada-002",
 }));
